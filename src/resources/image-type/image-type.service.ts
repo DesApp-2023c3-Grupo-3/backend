499a646b6ce215a278ae07048dbb3005ab544318
Luciano Garegnani <85763941+lucianogaregnani@users.noreply.github.com>
2023-08-27 14:59:51 +0000

    CRUD de ImageType realizado

--- src/resources/image-type/image-type.service.ts
+++ src/resources/image-type/image-type.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateImageTypeDto } from './dto/create-image-type.dto';
 import { UpdateImageTypeDto } from './dto/update-image-type.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ImageType } from 'src/entities/image-type.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ImageTypeService {
+  constructor(
+    @InjectRepository(ImageType)
+    private imageTypeRepository: Repository<ImageType>,
+  ) {}
+
   create(createImageTypeDto: CreateImageTypeDto) {
-    return 'This action adds a new imageType';
+    return this.imageTypeRepository.save(createImageTypeDto);
   }
 
   findAll() {
-    return `This action returns all imageType`;
+    return this.imageTypeRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} imageType`;
+    return this.imageTypeRepository.findOneByOrFail({ id });
   }
 
   update(id: number, updateImageTypeDto: UpdateImageTypeDto) {
-    return `This action updates a #${id} imageType`;
+    return this.imageTypeRepository.update({ id }, updateImageTypeDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} imageType`;
+    return this.imageTypeRepository.delete({ id });
   }
 }
