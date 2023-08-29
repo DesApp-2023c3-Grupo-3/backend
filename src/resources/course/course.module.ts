4d4e89fb6d80b7498336d082876b0324761a1d54
tomas0011 <tomasbenjamin117@gmail.com>
2023-08-29 17:38:05 -0300

    add crud course

--- src/resources/course/course.module.ts
+++ src/resources/course/course.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { CourseController } from './course.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { SocketModule } from 'src/plugins/socket/socket.module';
+import { Course } from 'src/entities/course.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Course])],
   controllers: [CourseController],
   providers: [CourseService, SocketModule],
 })
-export class CourseModule { }
+export class CourseModule {}
--- src/resources/course/course.service.ts
+++ src/resources/course/course.service.ts
@@ -1,33 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
 
 import { SocketService } from 'src/plugins/socket/socket.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Course } from 'src/entities/course.entity';
 
 @Injectable()
 export class CourseService {
   constructor(
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
     private readonly natsService: SocketService,
-  ) { }
+  ) {}
 
-  create(createCourseDto: CreateCourseDto) {
-    this.natsService.sendMessage('course', 'Este es un mensaje enviado desde CourseService.create')
-    return 'This action adds a new course';
+  public async create(createCourseDto: CreateCourseDto) {
+    const newCourse = this.courseRepository.create(createCourseDto);
+    const created = await this.courseRepository.save(newCourse);
+    this.natsService.sendMessage(
+      'course',
+      'Este es un mensaje enviado desde CourseService.create',
+    );
+    return created;
   }
 
-  findAll() {
-    return `This action returns all course`;
+  public async findAll() {
+    return this.courseRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} course`;
+  public async findOne(id: number) {
+    try {
+      return this.courseRepository.find({ where: { id } });
+    } catch (error) {
+      throw new HttpException('Course not found', HttpStatus.BAD_REQUEST);
+    }
   }
 
-  update(id: number, updateCourseDto: UpdateCourseDto) {
-    return `This action updates a #${id} course`;
+  public async update(id: number, updateCourseDto: UpdateCourseDto) {
+    try {
+      return this.courseRepository.update({ id }, updateCourseDto); // TODO: Revisar el output del update
+    } catch (error) {
+      throw new HttpException('Error on update', HttpStatus.BAD_REQUEST);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} course`;
+  public async remove(id: number) {
+    try {
+      return this.courseRepository.update(
+        { id },
+        {
+          id,
+          deletedAt: Date.now(), // TODO: Probar el borrado logico de @DeletedAtColumn
+        },
+      );
+    } catch (error) {
+      throw new HttpException('Error on delete', HttpStatus.BAD_REQUEST);
+    }
   }
 }
