39dcdd873d7ad561094ba980c0c18fa65df7682f
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2023-08-29 22:06:25 -0300

    CRUD ImageType creado

--- src/resources/image-type/image-type.controller.ts
+++ src/resources/image-type/image-type.controller.ts
@@ -10,27 +10,34 @@ import {
 import { ImageTypeService } from './image-type.service';
 import { CreateImageTypeDto } from './dto/create-image-type.dto';
 import { UpdateImageTypeDto } from './dto/update-image-type.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ImageTypeDto } from 'cartelera-unahur';
 
+@ApiTags('ImageType')
 @Controller('image-type')
 export class ImageTypeController {
   constructor(private readonly imageTypeService: ImageTypeService) {}
 
   @Post()
+  @ApiResponse({ type: ImageTypeDto })
   create(@Body() createImageTypeDto: CreateImageTypeDto) {
     return this.imageTypeService.create(createImageTypeDto);
   }
 
   @Get()
+  @ApiResponse({ type: Array<ImageTypeDto> })
   findAll() {
     return this.imageTypeService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({ type: ImageTypeDto })
   findOne(@Param('id') id: string) {
     return this.imageTypeService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiResponse({ type: ImageTypeDto })
   update(
     @Param('id') id: string,
     @Body() updateImageTypeDto: UpdateImageTypeDto,
@@ -39,6 +46,7 @@ export class ImageTypeController {
   }
 
   @Delete(':id')
+  @ApiResponse({ type: ImageTypeDto })
   remove(@Param('id') id: string) {
     return this.imageTypeService.remove(+id);
   }
