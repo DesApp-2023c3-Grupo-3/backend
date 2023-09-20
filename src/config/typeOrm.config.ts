4cb160f6f0c9c7ae32d605874985f92ca4ae877f
ValentinoChiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2023-09-20 20:33:27 -0300

    Se arreglan algunos parametros

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -11,7 +11,7 @@ const credentials = url
   ? { url }
   : {
       host: process.env.DATABASE_HOST || 'localhost',
-      port: parseInt(process.env.DATABASE_PORT, 10) || 5440,
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
       username: process.env.DATABASE_USERNAME || 'postgres',
       password: process.env.DATABASE_PASSWORD || 'admin1234',
       database: process.env.DATABASE_NAME || `contactos_${environment}`,
